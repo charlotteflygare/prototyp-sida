3642bdecf830794bd22ad2cc410bd5bef7f375a4
Charlotte Flygare <charlotte.flygare@gmail.com>
2018-12-21 09:51:19 +0100

    Design

--- code/src/components/hero/hero.js
+++ code/src/components/hero/hero.js
@@ -7,7 +7,10 @@ class Hero extends React.Component {
   render() {
     return (
       <div className="hero-container">
-        <img src="./images/skissbild_fapa.png" alt="fapa" />
+      <video muted="muted" autoplay="autoplay" loop="loop" width="1366" height="768">
+        <source src="./images/test.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+        </video>
       </div>
     )
   }
--- code/src/components/startpage/startpage.js
+++ code/src/components/startpage/startpage.js
@@ -3,18 +3,43 @@ import { HashRouter as Router, Route } from "react-router-dom"
 import Header from "../header/header.js"
 import Hero from "../hero/hero.js"
 import Footer from "../footer/footer.js"
-import "./startpage.scss"
+import "./startpage.css"
 
 class Startpage extends React.Component {
 
   render() {
     return (
-      <div className="startpage-container">
-        <Hero />
-        <Header />
+      <div>
+          <Hero />
+          <div className="wrapper">
+
+          <Header />
+          <div className="grid-container">
+            <div className="item1">
+              <div className="item1-image">
+                <img src="./images/sl.jpg" alt="SL" />
+              </div>
+            </div>
+
+            <div className="item2">
+              <h2>SL Skyltsystem och wayfinding</h2>
+              <p> Att hitta rätt i SL-systemet kan vara knepigt.
+              Vi har hittat mer än tjugo olika grafiska system i
+              tunnelbana, pendeltåg, spårvagn, buss och båt. Inget
+              är på engelska och inget följer SL:s visuella identitet.</p>
+            </div>
+            <div className="item3">
+              <img src="./images/ob.jpg" alt="SL" />
+            </div>
+
+
+
+
+        </div>
       </div>
+      </div>
+
     )
   }
 }
-
 export default Startpage
