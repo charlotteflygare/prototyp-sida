eb6be6dd9603ffc12f9fd8a07fa0d284ef8defdc
Charlotte Flygare <charlotte.flygare@gmail.com>
2019-01-07 15:51:30 +0100

    Made up a grid

--- code/src/components/startpage/startpage.js
+++ code/src/components/startpage/startpage.js
@@ -15,24 +15,51 @@ class Startpage extends React.Component {
 
           <Header />
           <div className="grid-container">
-            <div className="item1">
-              <div className="item1-image">
-                <img src="./images/sl.jpg" alt="SL" />
-              </div>
+            <div className="item image-sl">
+              <img src="./images/sl.jpg" alt="SL" />
             </div>
 
-            <div className="item2">
-              <h2>SL Skyltsystem och wayfinding</h2>
-              <p> Att hitta rätt i SL-systemet kan vara knepigt.
-              Vi har hittat mer än tjugo olika grafiska system i
-              tunnelbana, pendeltåg, spårvagn, buss och båt. Inget
-              är på engelska och inget följer SL:s visuella identitet.</p>
+            <div className="item image-sl-2">
+              <img src="./images/SL3.jpg" alt="SL" />
             </div>
-            <div className="item3">
-              <img src="./images/ob.jpg" alt="SL" />
+
+            <div className="item text">
+              <h2>Sl</h2>
+              <p>Visuell identitet och trafikinformation<span>
+                 En färgsprakande och användarvänlig lösning som möter
+                 de höga krav på tillgänglighet som huvudstadens kollektivtrafik
+                 kräver. Nu finns till exempel all information både på svenska
+                 och engelska. Och den nya spårtrafikkartan är, enligt vår mening,
+                 ett konstverk i sig.</span>
+              </p>
             </div>
 
 
+            <div className="item image-ob-logo">
+              <img src="./images/o.jpg" alt="Orebro" />
+            </div>
+
+            <div className="item image-ob">
+              <img src="./images/ob.jpg" alt="Orebro" />
+            </div>
+
+            <div className="item image-z">
+              <img src="./images/z2.gif" alt="Orebro" />
+            </div>
+
+            <div className="item image-z-2">
+              <img src="./images/z.gif" alt="Orebro" />
+            </div>
+
+            <div className="item text-z">
+              <p>Visuell identitet och trafikinformation<span>
+                 En färgsprakande och användarvänlig lösning som möter
+                 de höga krav på tillgänglighet som huvudstadens kollektivtrafik
+                 kräver. Nu finns till exempel all information både på svenska
+                 och engelska. Och den nya spårtrafikkartan är, enligt vår mening,
+                 ett konstverk i sig.</span>
+              </p>
+            </div>
 
 
         </div>
